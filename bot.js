694785604abef0f273bc86e162efc1688a5a0586
Nahida4479 <szymon.cyb13@gmail.com>
2026-07-08 23:27:44 +0200

    HP mammona 250, hp za gracza 100

--- bot.js
+++ bot.js
@@ -795,8 +795,8 @@ async function getUstawienia(userId, guildId) {
 // Mammon - event bossa
 
 const MAMMON_SCIEZKA_OBRAZKA = "./Gra/mammon/mammon.jpg";
-const MAMMON_HP_BAZA = 400;
-const MAMMON_HP_ZA_GRACZA = 200;
+const MAMMON_HP_BAZA = 250;
+const MAMMON_HP_ZA_GRACZA = 100;
 const MAMMON_ATAK_OBRAZENIA = 10;
 const MAMMON_ULT_OBRAZENIA = MAMMON_ATAK_OBRAZENIA * 3;
 const MAMMON_COOLDOWN_ATAK_MS = 1000;
